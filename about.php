f85f5d66fca973b3ebfb25ed6974af84c5cf1aa9
Dhanush.B <94157320+dhanush8020@users.noreply.github.com>
2024-11-21 18:23:57 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,60 @@
+<?php include('./constant/layout/head.php');?>
+<?php include('./constant/layout/header.php');?>
+
+<?php include('./constant/layout/sidebar.php');?>
+
+
+<div class="page-wrapper">
+	<div class="container-fluid">
+		<div class="card">
+			<div class="card-body">
+				<div class="about-head text-center mb-5">
+					<h1 style="color: black;" class="mb-2 mt-4 f-s-50 ">About Food Point.</h1>
+					<span>A Catering Management Website</span>
+				</div>
+				<div class="about">
+					<div class="row">
+						<!-- <div class="col-md-3">
+							<img src="assets/myimages/profile.jpg" class="img-fluid">
+						</div> --> 
+						<!--  Author : Mayuri K. 
+ for any PHP, Codeignitor, Laravel OR Python work contact me at email mayuri.infospace@gmail.com  
+ Visit website - www.mayurik.com -->
+
+
+ <h3 class="mb-1" style="text-align: justify; color: gray;">About Us </h3>
+ <h3 class="mb-1" style="text-align: justify; color: gray;">Welcome to Food Point! We believe that every meal should be a memorable experience. Our catering management system is designed to simplify the process of ordering and managing food services for events of all sizes, from corporate gatherings to private parties.</h3>
+						
+ 
+<h3 class="mb-1" style="text-align: justify; color: gray;">Our Mission </h3>
+ <h3 class="mb-1" style="text-align: justify; color: gray;">To provide a seamless catering experience that meets the diverse needs of our clients while delivering delicious, high-quality food and exceptional service.</h3>
+ 
+ <h3 class="mb-1" style="text-align: justify; color: gray;">Our Vision </h3>
+ <h3 class="mb-1" style="text-align: justify; color: gray;">Welcome to Food Point! We believe that every meal should be a memorable experience. Our catering management system is designed to simplify the process of ordering and managing food services for events of all sizes, from corporate gatherings to private parties.</h3>
+ 
+ 
+ <h3 class="mb-1" style="text-align: justify; color: gray;">What we Offer </h3> 
+ <h3 class="mb-1" style="text-align: justify; color: gray;">Easy online ordering and menu customization,Real-time tracking of orders and delivery,Support for dietary preferences and restrictions,Event planning assistance to ensure everything runs smoothly,Feedback and review system to continually improve our offerings.
+</h3>
+
+ <h3 class="mb-1" style="text-align: justify; color: gray;">Contact Us </h3>
+ <h3 class="mb-1" style="text-align: justify; color: gray;">If you have any questions or would like to learn more about our services, please don't hesitate to contact us. We look forward to serving you in future!.
+	Contact us at foodpoint@gmail.com or via mobile phone 12234567890.  
+</h3>
+<h3 class="mb-1" style="text-align: justify; color: gray;">Thank you</h3>
+
+
+
+					</div>
+					
+				</div>
+			</div>			
+		</div>
+	</div>
+</div> -->
+
+
+<?php include('./constant/layout/footer.php');?>
+<!--  Author Name- Mayuri K. 
+ for any PHP, Codeignitor, Laravel OR Python work contact me at mayuri.infospace@gmail.com  
+ Visit website - www.mayurik.com -->
